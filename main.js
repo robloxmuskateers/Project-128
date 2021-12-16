ff279898c9b0db1c05bb124d073c107d1740a01b
robloxmuskateers <79553090+robloxmuskateers@users.noreply.github.com>
2021-12-15 22:40:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Hsong = "";
+Psong = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload() {
+    Hsong = loadSound("HarryPotter.mp3");
+    Psong = loadSound("PeterPan.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Operating");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "; Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "; Right Wrist Y = " + rightWristY);
+    }
+}
